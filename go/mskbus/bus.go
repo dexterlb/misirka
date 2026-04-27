afb3af238c1936a4a8ce66786c5a39aea3b8c357
dexterlb <dexterlb@qtrp.org>
2026-04-27 17:55:14 +0300

    apply and dedup for buses

--- go/mskbus/bus.go
+++ go/mskbus/bus.go
@@ -13,6 +13,7 @@ type BusOf[T any] struct {
 	lastVal     T
 	subscribers map[uint]func(T)
 	n           uint
+	appliers    []func(T, T) (T, bool)
 }
 
 func New[T any]() *BusOf[T] {
@@ -39,11 +40,20 @@ func (b *BusOf[T]) Unsubscribe(id uint) {
 
 func (b *BusOf[T]) Send(x T) {
 	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	for _, applier := range b.appliers {
+		newVal, keep := applier(b.lastVal, x)
+		if keep {
+			return
+		}
+		x = newVal
+	}
+
 	b.lastVal = x
 	for i := range b.subscribers {
 		b.subscribers[i](x)
 	}
-	b.mutex.Unlock()
 }
 
 func (b *BusOf[T]) Get() T {
@@ -59,4 +69,33 @@ func (b *BusOf[T]) GetT() interface{} {
 
 func (b *BusOf[T]) SubscribeT(handler func(interface{})) uint {
 	return b.Subscribe(func(x T) { handler(x) })
+}
+
+// Apply adds a transormation that will be applied each time a new
+// value is sent to the bus. The function will be supplied two arguments:
+//   - The previous value that was sent through the bus (after all applied transformations)
+//   - The current new value
+//
+// The transformation should return:
+//   - The transformed new value
+//   - A boolean whether to keep the value (if false the value will be discarded)
+//
+// This mechanism can be used to implement simple operations like
+// deduplication, moving average, filtering, etc but is not as expressive
+// as proper functional programming primitives like map/reduce that allow
+// different input/output types
+func (b *BusOf[T]) Apply(f func(T, T) (T, bool)) *BusOf[T] {
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+
+	b.appliers = append(b.appliers, f)
+	return b
+}
+
+// DedupBy makes the bus deduplicate values by comparing them
+// with the given test
+func (b *BusOf[T]) DedupBy(f func(T, T) bool) *BusOf[T] {
+	return b.Apply(func(prev T, x T) (T, bool) {
+		return x, !f(prev, x)
+	})
 }
