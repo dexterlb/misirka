113733f6cb0f0e24d5a9b3f01a6d1071fbc01013
Albert Stefanov <aastefanov@outlook.com>
2026-09-05 11:48:54 +0300

    Break public API to help unbundling
    Having everything imported in `index.ts` makes everything be directly
    imported when used as a library.
    We shouldn't include MQTT.js in the user bundles when we don't create
    any instance at all

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,6 @@
-export { WSClient } from "./wsclient"
-export { MQTTClient } from "./mqttclient"
 export { SubClient } from "./subclient"
-
-export { MisirkaClient } from "./client"
 export type { SubscribeToken } from "./client"
-
 export type { Schema } from "./schemas"
+
+import {MisirkaClient} from "./client";
+export default MisirkaClient
